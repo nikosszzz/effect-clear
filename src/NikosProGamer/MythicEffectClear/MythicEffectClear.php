709e650f0e33d66655526b726cf6ecda2ca3d8e4
NikosProGamer <53112175+NikosProGamer@users.noreply.github.com>
2020-03-31 19:32:29 +0300

    Add files via upload

--- src/NikosProGamer/MythicEffectClear/MythicEffectClear.php
+++ src/NikosProGamer/MythicEffectClear/MythicEffectClear.php
@@ -1,1 +1,44 @@
+<?php
 
+namespace NikosProGamer\MythicEffectClear;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginManager;
+use pocketmine\Player;
+
+class MythicEffectClear extends PluginBase implements Listener{
+    
+    public function onEnable()
+    {
+        $this->getLogger()->info("§d§lMEC §r§6Enabled!");
+        $this->registerEvents();
+
+    }
+
+    public function registerEvents()
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+        if (strtolower($command->getName()) == "bless") {
+            if ($sender->hasPermission("bless.cmd")) {
+                foreach ($sender->getEffects() as $effect) {
+                    if ($effect->getType()->isBad()) {
+                        $sender->removeEffect($effect->getId());
+                        $sender->sendMessage("§6§l§oYou have been blessed from bad effects!");
+                    }elseif(!$sender->hasPermission("bless.cmd")){
+                        $sender->sendMessage("§cYou do not have permission to use §d§lMEC§r§c! Buy a §6§lRank§r §cto gain access to it!");                 
+
+
+                    }
+                }
+            }
+        }
+return true;
+    }
+}
